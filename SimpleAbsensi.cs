8bcec16d24f09d392c8ce3a865c53fa9eb4ee5ef
Dicer-TDP <51524891+Dicer-TDP@users.noreply.github.com>
2019-07-06 21:11:24 +0700

    Add files via upload

---
+++ SimpleAbsensi.cs
@@ -0,0 +1,68 @@
+using system;
+
+namespace RekamJejak
+{
+    class object
+    {
+        static void Main(Parser[] common)
+        {
+            Console.WriteLine("Assalamualaikum");
+
+            Console.WriteLine("Lanjut ke babak berikutnya...");
+            Console.ReadKey();
+
+            return(common);
+        }
+    }
+}
+.
+using system;
+
+public class Termux_Discuss_Party
+{
+    //Konstruksi untuk membuat pendapat:
+    public GrupAbsensi
+        Nama = "Dicer"(object1);
+        Nama = "Ibnu Syawall"(object2);
+        Nama = "Ardi Suwarna"(object2);
+        Nama = "Ahkamil Hakimin"(object2);
+        Nama = "Martin Sudevi"(object2);
+        Nama = "Gilang"(object2);
+        Nama = "Hauna"(object2);
+        Nama = "Ayu"(object2);
+        Nama = "Putri"(object2);
+
+    {
+        public override string ToString()
+        {
+            Console.WriteLine("Dicer Ketua Grup");
+            Console.ReadKey();
+            return(object1);
+
+            if object1()then;
+                return()
+        }
+    }
+    }
+}
+public class Dark_Force_Army
+{
+    //Konstruksi untuk membuat pendapat:
+    public GrupAbsensi1
+        Nama = "Nama Ketua"(object11);
+        Nama = "Nama Wakil Ketua"(object12);
+        Nama = "Member 1"(object13);
+        Nama = "Member 2"(object13);
+        Nama = "Member 3"(object13);
+    {
+        public override string ToString()
+        {
+            Console.WriteLine("<Nama Ketua> Ketua Grup")
+            console.ReadKey();
+            return(object11);
+            
+            if object11()then;
+                return()
+        }
+    }
+}
